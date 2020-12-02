286bbfe424a0f3672754931804ba585ca62d2e36
David Barthelemy <>
2020-12-02 09:15:41 +0100

    update day1

--- day1/main.py
+++ day1/main.py
@@ -14,7 +14,6 @@ def main_2():
     data = read_file()
     s_data = set(data)
     len_ = len(data) - 1
-    cache = set()
     for idx, elem in enumerate(data):
         if idx == len_:
             break
@@ -25,21 +24,14 @@ def main_2():
                 continue
             next_ = data[j + 1]
             p_sum = elem + next_
-            if p_sum in cache:
-                j += 1
-                continue
-            cache.add(p_sum)
-            if p_sum > 2020:
-                j += 1
-                continue
             if 2020 - p_sum in s_data:
                 return elem * next_ * (2020 - p_sum)
             j += 1
     return None
-# result is 6964490
+
 if __name__ == "__main__":
-    #number = main_1()
-    #print(number)
+    number = main_1()
+    print(number)
 
     number = main_2()
     print(number)

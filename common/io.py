539d7ad6b450cadf1bea58c7a29cae07b98edeee
David Barthelemy <david.barthelemy@blablacar.com>
2020-12-03 18:44:23 +0100

    update io

--- common/io.py
+++ common/io.py
@@ -1,5 +1,5 @@
-def read_file(f=None):
-    with open('/Users/d.barthelemy/AdventOfCode2020/day2/input.txt', 'r') as f:
+def read_file(filename, f=None):
+    with open(filename, 'r') as f:
         for line in f:
             yield f(line) if f else line
             

c72f6e38b8f161a7d4ae605056e440a1676c9870
David Barthelemy <davidbarthelemy28@gmail.com>
2020-12-13 14:27:08 +0100

    second part

--- day13/main.py
+++ day13/main.py
@@ -14,8 +14,20 @@ def main1():
     bus_id = next(x[0] for x in matrix if x[1] == time)
     print((time - my_time) * bus_id)
 
+# Thanks https://brilliant.org/wiki/chinese-remainder-theorem/ and reddit hints
+def crt(dividers, remainders):
+    _sum = 0
+    product = math.prod(dividers)
+    for a, b in zip(dividers, remainders):
+        _tmp = product // a
+        _sum += b * _tmp * pow(_tmp, -1, a)
+    return _sum % product
+
 def main2():
-    lines = read_file('input.txt')
+    lines = list(read_file('input.txt'))
+    buses = [(idx, int(_id)) for idx, _id in enumerate(lines[1].split(',')) if _id != 'x']
+    reminders = [value-idx for idx, value in buses] 
+    print(crt([x[1] for x in buses], reminders))
     
 if __name__ == "__main__":
     main1()

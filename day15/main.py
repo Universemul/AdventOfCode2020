f0c98f6e9c5efbd3649437beffd57f5b144b6f29
David Barthelemy <david.barthelemy@blablacar.com>
2020-12-15 13:35:47 +0100

    don't use list

--- day15/main.py
+++ day15/main.py
@@ -5,18 +5,29 @@ def read_file(filename, func=None):
         for line in f:
             return func(line.strip()) if func else line.strip()
 
+class Test():
+    def __init__(self):
+        self.len = 0
+        self.last = None
+        self.last_last = None
+
+    def insert(self, value):
+        self.last_last = self.last
+        self.last = value
+        self.len += 1
+
 def main(iteration):
     numbers = [int(x) for x in list(read_file('input.txt').split(','))]
-    result = {x: [] for x in numbers}
+    result = dict()
     prev = None
     for idx, key in enumerate(numbers):
         prev = key
-        result[key].append(idx)
+        result.setdefault(key, Test()).insert(idx)
     for turn in range(len(numbers), iteration):
         value = 0
-        if len(result[prev]) > 1:
-            value = result[prev][-1] - result[prev][-2]
-        result.setdefault(value, []).append(turn)
+        if result[prev].len > 1:
+            value = result[prev].last - result[prev].last_last
+        result.setdefault(value, Test()).insert(turn)
         prev = value
     print(prev)
 

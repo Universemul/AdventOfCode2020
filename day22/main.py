90c8da4b6064f1eeee47bb6a42887da1664db2ce
David Barthelemy <davidbarthelemy28@gmail.com>
2020-12-22 21:12:46 +0100

    day 22 part 2

--- day22/main.py
+++ day22/main.py
@@ -44,8 +44,35 @@ def main1():
     p = player1 or player2
     print(sum(x * (max_cards - i) for i, x in enumerate(p)))
 
+def new_game(p1, p2):
+    seen = set()
+    while len(p1) and len(p2):
+        game = f"{str(p1)}{str(p2)}"
+        if game in seen:
+            return (1, p1)
+        seen.add(game)
+        card1 = p1.pop(0)
+        card2 = p2.pop(0)
+        if len(p1) >= card1 and len(p2) >= card2:
+            r_winner, _ = new_game(deepcopy(p1[:card1]), deepcopy(p2[:card2]))
+        else:
+            r_winner = 1 if card1 > card2 else 2
+        p = p1 if r_winner == 1 else p2
+        p.append(card1 if r_winner == 1 else card2)
+        p.append(card2 if r_winner == 1 else card1)
+    winner = (1, p1)
+    if len(p1) == 0:
+        winner = (2, p2)
+    return winner
+
+
 def main2():
     lines = read_file(INPUT_FILE)
+    player1, player2 = get_players(lines)
+    winner, player = new_game(player1, player2)
+    max_cards = len(player)
+    print(sum(x * (max_cards - i) for i, x in enumerate(player)))
+
     
 if __name__ == "__main__":
     main1()
